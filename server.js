da76efe59592acad0d4fbd388fb5a8e3ca58841a
Adesh <wandaleadesh1211@gmail.com>
2026-03-29 19:01:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -296,11 +296,11 @@ app.get("/api/admin/stats", authMiddleware, (req, res) => {
 });
 
 // 🔥 SERVE FRONTEND BUILD (IMPORTANT)
-app.use(express.static(path.join(__dirname, "build")));
+//app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+//app.get("*", (req, res) => {
+//res.sendFile(path.join(__dirname, "build", "index.html"));
+//});
 
 // ── START SERVER ──
 const PORT = process.env.PORT || 5000;
